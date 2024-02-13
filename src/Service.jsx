fe8fb3c3f9c124dc376ce1c103e60dbf44281b1b
gajjoshi <gajbjoshi@gmail.com>
2024-02-13 19:16:18 +0530

    cheers

--- src/Service.jsx
+++ src/Service.jsx
@@ -3,98 +3,107 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./Services.css";
-import a1 from "./assets/glass.png"
-import a2 from "./assets/glass2.png"
-import a3 from "./assets/glass3.png"
-import a4 from "./assets/glass4.png"
-
-
-
+import a1 from "./assets/glass.png";
+import a2 from "./assets/glass2.png";
+import a3 from "./assets/glass3.png";
+import a4 from "./assets/glass4.png";
+import b1 from "./assets/wp1.png";
+import b2 from "./assets/wp2.png";
+import b3 from "./assets/wp3.png";
 
 const data1 = [
-  { 
-    id: 1, 
-    title: "CRACK FIX SOLUTION", 
-    description: "Image", 
+  {
+    id: 1,
+    title: "CRACK FIX SOLUTION",
+    description: "Image",
     caption: (
       <>
         <p>Pakaging Size : 5-20KG</p>
         <p>Physical State : Powder</p>
       </>
     ),
-    image:a1
+    image: a1
   },
-
-
-
-
-
-  { 
-    id: 2, 
-    title: "AR GLASS FIBRE", 
-    description: "Image", 
+  {
+    id: 2,
+    title: "AR GLASS FIBRE",
+    description: "Image",
     caption: (
       <>
         <p>Pakaging GSM : 300-450</p>
         <p>Physical State : Powder</p>
-        <p>Packaging Size :	20-25 Kg</p>
+        <p>Packaging Size : 20-25 Kg</p>
         <p>Density : 100 kg/m3</p>
-
       </>
     ),
-    image:a2
+    image: a2
   },
-
-
-
-  { 
-    id: 3, 
-    title: "White Chopped Strands Glass Fiber, For Concrete Reinforcement", 
-    description: "Image", 
+  {
+    id: 3,
+    title: "White Chopped Strands Glass Fiber, For Concrete Reinforcement",
+    description: "Image",
     caption: (
       <>
-        <p>Glass Fiber Type	: Chopped Strands</p>
-        <p>Usage/Application	: Concrete Reinforcement</p>
-        <p>Size	
-12-13 Micron</p>
-        <p>Material	Glass : Fiber</p>
-
+        <p>Glass Fiber Type : Chopped Strands</p>
+        <p>Usage/Application : Concrete Reinforcement</p>
+        <p>Size 12-13 Micron</p>
+        <p>Material Glass : Fiber</p>
       </>
     ),
-    image:a3
+    image: a3
   },
-
-
-  { 
-    id: 4, 
-    title: "FIBERLESS GLASS MESH", 
-    description: "Image", 
+  {
+    id: 4,
+    title: "FIBERLESS GLASS MESH",
+    description: "Image",
     caption: (
       <>
-        <p>GSM :	60-80 GSM</p>
-        <p>Weight	: 30-100 gram per square meter</p>
-        <p>Thickness	: 0.1 to 1 mm</p>
-        <p>Length :	20-60 m</p>
-
+        <p>GSM : 60-80 GSM</p>
+        <p>Weight : 30-100 gram per square meter</p>
+        <p>Thickness : 0.1 to 1 mm</p>
+        <p>Length : 20-60 m</p>
       </>
     ),
-    image:a4
+    image: a4
+  }
+];
+
+const data2 = [
+  {
+    id: 1,
+    title: "WPE-55 WATERPROOFING MEMBRANE",
+    description: "Image",
+    caption: (
+      <>
+        <p>Pattern : Plain</p>
+        <p>Thickness : 1-3 mm</p>
+        <p>Length : 50-100 m</p>
+        <p>Width : 1-2 m</p>
+      </>
+    ),
+    image: b1
   },
-
-
-
-
-
-
-
-
-
-
-
-  { id: 3, title: "P3", description: "DS", caption: "Caption 3" ,image:{a1}},
-  { id: 4, title: "P4", description: "DS", caption: "Caption 4" ,image:{a1}},
-  { id: 5, title: "P5", description: "DS", caption: "Caption 5",image:{a1} },
-  { id: 6, title: "P6", description: "DS", caption: "Caption 6",image:{a1} },
+  {
+    id: 2,
+    title: "SMART CARE MEMBRANE",
+    description: "Image",
+    caption: (
+      <>
+        <p>Brand : WPE-55</p>
+        <p>Material : pp</p>
+        <p>Thickness : 1 mm</p>
+        <p>Size : 1.2mtrs x 100mtrs</p>
+      </>
+    ),
+    image: b2
+  },
+  {
+    id: 3,
+    title: "PP WATERPROOFING MEMBRANE",
+    description: "Image",
+    caption: <></>,
+    image: b3
+  }
 ];
 
 export default function Service() {
@@ -116,10 +125,11 @@ export default function Service() {
     setShowMore(!showMore);
     setShowLess(false);
   };
-const handleToggleLess = () => {
-  setShowLess(!showLess);
-  setShowMore(false); // Set showMore to false when showing less
-};
+
+  const handleToggleLess = () => {
+    setShowLess(!showLess);
+    setShowMore(false); // Set showMore to false when showing less
+  };
 
   const settings = {
     dots: false,
@@ -128,16 +138,16 @@ const handleToggleLess = () => {
     autoplay: true,
     autoplaySpeed: 0,
     cssEase: "linear",
-    slidesToShow: 3,  
+    slidesToShow: 3,
     slidesToScroll: 1,
     responsive: [
       {
         breakpoint: 768,
         settings: {
-          slidesToShow: 1,
-        },
-      },
-    ],
+          slidesToShow: 1
+        }
+      }
+    ]
   };
 
   return (
@@ -154,7 +164,6 @@ const handleToggleLess = () => {
             GLASS FIBRE
           </h1>
           <div className="card-slider-container">
-            
             <Slider {...settings} className="card-slider">
               {data1.map((item) => (
                 <div key={item.id} className="flip-card-container">
@@ -166,13 +175,10 @@ const handleToggleLess = () => {
                   >
                     <div className="flip-card-inner">
                       <div className="flip-card-front">
-                        <h2 className="title" >{item.title}</h2>
-
+                        <h2 className="title">{item.title}</h2>
                       </div>
                       <div className="flip-card-back">
-                        {/* <h2 className="title">{item.description}</h2> */}
                         <img src={item.image} alt={item.title} />
-
                       </div>
                     </div>
                   </div>
@@ -181,12 +187,14 @@ const handleToggleLess = () => {
               ))}
             </Slider>
           </div>
+          <div style={{marginTop:"50px"}}>
+          </div>
           <h1 className="text-6xl md:text-3xl lg:text-7xl uppercase text-center text-white sm:text-2xl py-0">
-          Waterproofing Membrane
+                Waterproofing Membrane
               </h1>
               <div className="card-slider-container">
                 <Slider {...settings} className="card-slider">
-                  {data1.slice(3).map((item) => (
+                  {data2.map((item) => (
                     <div key={item.id} className="flip-card-container">
                       <div
                         className={`flip-card ${
@@ -199,7 +207,7 @@ const handleToggleLess = () => {
                             <h2 className="title">{item.title}</h2>
                           </div>
                           <div className="flip-card-back">
-                            <h2 className="title">{item.description}</h2>
+                            <img src={item.image} alt={item.title} />
                           </div>
                         </div>
                       </div>
@@ -211,11 +219,11 @@ const handleToggleLess = () => {
           {showMore && (
             <>
               <h1 className="text-6xl md:text-3xl lg:text-7xl uppercase text-center text-white sm:text-2xl py-0">
-              Prefinished Siding
+                Waterproofing Membrane
               </h1>
               <div className="card-slider-container">
                 <Slider {...settings} className="card-slider">
-                  {data1.slice(3).map((item) => (
+                  {data2.map((item) => (
                     <div key={item.id} className="flip-card-container">
                       <div
                         className={`flip-card ${
@@ -228,7 +236,7 @@ const handleToggleLess = () => {
                             <h2 className="title">{item.title}</h2>
                           </div>
                           <div className="flip-card-back">
-                            <h2 className="title">{item.description}</h2>
+                            <img src={item.image} alt={item.title} />
                           </div>
                         </div>
                       </div>
@@ -237,80 +245,19 @@ const handleToggleLess = () => {
                   ))}
                 </Slider>
               </div>
-              <h1 className="text-6xl md:text-3xl lg:text-7xl uppercase text-center text-white sm:text-2xl py-0">
-              Asphalt Roofing Shingles
-              </h1>
-              <div className="card-slider-container">
-                <Slider {...settings} className="card-slider">
-                  {data1.slice(3).map((item) => (
-                    <div key={item.id} className="flip-card-container">
-                      <div
-                        className={`flip-card ${
-                          flippedCards.includes(item.id) ? "flipped" : ""
-                        }`}
-                        onClick={() => handleCardClick(item.id)}
-                      >
-                        <div className="flip-card-inner">
-                          <div className="flip-card-front">
-                            <h2 className="title">{item.title}</h2>
-                          </div>
-                          <div className="flip-card-back">
-                            <h2 className="title">{item.description}</h2>
-                          </div>
-                        </div>
-                      </div>
-                      <p className="caption">{item.caption}</p>
-                    </div>
-                  ))}
-                </Slider>
-              </div>
-
-
-              <h1 className="text-6xl md:text-3xl lg:text-7xl uppercase text-center text-white sm:text-2xl py-0">
-              Self Closing Doors
-              </h1>
-              <div className="card-slider-container">
-                <Slider {...settings} className="card-slider">
-                  {data1.slice(3).map((item) => (
-                    <div key={item.id} className="flip-card-container">
-                      <div
-                        className={`flip-card ${
-                          flippedCards.includes(item.id) ? "flipped" : ""
-                        }`}
-                        onClick={() => handleCardClick(item.id)}
-                      >
-                        <div className="flip-card-inner">
-                          <div className="flip-card-front">
-                            <h2 className="title">{item.title}</h2>
-                          </div>
-                          <div className="flip-card-back">
-                            <h2 className="title">{item.description}</h2>
-                          </div>
-                        </div>
-                      </div>
-                      <p className="caption">{item.caption}</p>
-                    </div>
-                  ))}
-                </Slider>
-              </div>
-
-
-
-              
-
-
-
-
-
-
-
-              <button onClick={handleToggleLess} className="text-white py-2 px-4 rounded bg-blue-500 hover:bg-blue-600">
+              <button
+                onClick={handleToggleLess}
+                className="text-white py-2 px-4 rounded bg-blue-500 hover:bg-blue-600"
+              >
                 Show Less
               </button>
             </>
           )}
           {!showMore && !showLess && (
-            <button onClick={handleToggleProducts} className="text-white py-2 px-4 rounded bg-blue-500 hover:bg-blue-600">
+            <button
+              onClick={handleToggleProducts}
+              className="text-white py-2 px-4 rounded bg-blue-500 hover:bg-blue-600"
+            >
               Load More
             </button>
           )}
